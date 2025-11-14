d5794e7f8420eabf2e29834b70c2d9273e83febd
Yati-21 <yati.amity@gmail.com>
2025-11-14 17:47:31 +0530

    AddedFiles for service

--- 14Nov25/springDemo/src/main/java/com/chubb/controller/OrderController.java
+++ 14Nov25/springDemo/src/main/java/com/chubb/controller/OrderController.java
@@ -2,21 +2,26 @@ package com.chubb.controller;
 import com.chubb.request.Order;
 import com.chubb.service.OrderService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController    // makes this class as spring bean
 public class OrderController   //receive http req
 {
+	@Autowired
 	OrderService service;
 	
 	@GetMapping("/order")   // add a unique path ()
 	String getOrder()
 	{
+//		log.debug("logger added");
 		return "hello";
 	}
 	
--- 14Nov25/springDemo/src/main/java/com/chubb/service/OrderService.java
+++ 14Nov25/springDemo/src/main/java/com/chubb/service/OrderService.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Service;
 
 import com.chubb.request.Order;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class OrderService //to implement business logic
 {
 	public void insertOrder(Order order) 
 	{
+		order.setTotalAmount(order.getPrice()*order.getQuantity());
 		System.out.println(order);
+	
+//		log.debug(order.toString());
 	}
 
 }
