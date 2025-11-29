8e657708928749b22e4f4b9b9acf3c2e981581ee
Yati-21 <yati.amity@gmail.com>
2025-11-29 10:42:47 +0530

    removed wemvc dependency for api gateway - causing error

--- 28Nov25/ApiGateway/src/test/java/com/gateway/ApiGatewayApplicationTests.java
+++ 28Nov25/ApiGateway/src/test/java/com/gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.gateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.gateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
